10c7c2d3ad80dcafa83bba71605c07b942ce2cfe
Beckyshee <wanjirubecky.rw@gmail.com>
2023-10-30 15:59:20 +0300

    first commit

---
+++ Friend-or-foe/friend_foe.py
@@ -0,0 +1,7 @@
+def friend(x):
+    x = ["Ryan", "Kieran", "Jason", "Yous"]
+    new_friends = []
+    for str in x:
+        if len(str) == 4:
+            return new_friends.append(str)
+    return new_friends[0:]
